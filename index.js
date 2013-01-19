11df9a530fdca2991c656efdb42683beea5d54d8
Antti Sykäri <antti.sykari@gmail.com>
2013-01-19 10:59:17 +0200

    Make it modular, fix it, and add tests.

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
+function log(message) {
+	var self = log;
+	return self.output(self.date() + message);
+}
 
+log.e = function(message) {
+	return this.output(this.date() + '[error] ' + message);
+}
+
+log.i = function(message) {
+	return this.output(this.date() + '[info] ' + message);
+}
+
+log.d = function(message) {
+	return this.output(this.date() + '[debug] ' + message);
+}
+
+// Return date plus a space.
+// Will be printed before each log line.
 function date() {
 	return new Date().toISOString().replace(/[TZ]/g, ' ');
 }
 
-function log(message) {
-	console.log(date() + message);
+// Outputs the message.
+function output(message) {
+	return console.log(message);
 }
 
-log.e = function(message) {
-	console.log(date() + '[error] ' + message);
-}
-
-log.i = function(message) {
-	console.log(date() + '[info] ' + message);
-}
-
-log.d = function(message) {
-	console.log(date() + '[info] ' + message);
-}
+log.date = date;
+log.output = output;
 
 module.exports = log;
-
